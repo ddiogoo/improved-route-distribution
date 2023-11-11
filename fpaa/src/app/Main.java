09742b370369058b0518efe59df700f4ceece2a4
diogomsz <diogomrtsa@gmail.com>
2023-11-11 18:16:19 -0300

    Update Main.java

--- fpaa/src/app/Main.java
+++ fpaa/src/app/Main.java
@@ -1,7 +1,21 @@
 package app;
 
+import java.util.List;
+
+import source.GeradorDeProblemas;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		List<int[]> conjunto = GeradorDeProblemas.geracaoDeRotas(2, 6, 10);
+		
+		int i = 1;
+		for(int[] c : conjunto) {
+			System.out.println("Elementos na " + i + "º posição do conjunto: ");
+			for(int v : c) {
+				System.out.print(v + "; ");
+			}
+			System.out.println();
+			i++;
+		}
 	}
 }
