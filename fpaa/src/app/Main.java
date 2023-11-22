52eec249c7cf2d1f4659ec863221d4bcfd86c53a
diogomsz <diogomrtsa@gmail.com>
2023-11-21 21:24:19 -0300

    Algoritmo guloso
    Implementando algoritmo guloso

--- fpaa/src/app/Main.java
+++ fpaa/src/app/Main.java
@@ -1,7 +1,26 @@
 package app;
 
+import java.util.List;
+
+import generators.GeradorDeProblemas;
+import source.GreedyAlgorithm;
+
 public class Main {
 	public static void main(String[] args) {
-
+		greedy();
+	}
+	
+	public static void greedy() {
+		List<int[]> sets = GeradorDeProblemas.geracaoDeRotas(6, 10, 0.5);
+		
+		for(int[] routes : sets) {
+			GreedyAlgorithm greedy = new GreedyAlgorithm(routes, 3);
+			greedy.distributeRoutesWithGreedy();
+			
+			int[] r = greedy.getRoutes();
+			for(int route : r) {
+				System.out.println(route);
+			}
+		}
 	}
 }
