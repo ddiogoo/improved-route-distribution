7f6b43974fca934654e83af2ee5e8096a76356e1
diogomsz <diogomrtsa@gmail.com>
2023-12-09 16:50:34 -0300

    Update Main.java

--- fpaa/src/App/Main.java
+++ fpaa/src/App/Main.java
@@ -13,7 +13,12 @@ import Geradores.GeradorDeProblemas;
 public class Main {
 	private static Map<Integer, List<Long>> hashMapPrimeiroGuloso = new HashMap<Integer, List<Long>>();
 	private static Map<Integer, List<Long>> hashMapSegundoGuloso = new HashMap<Integer, List<Long>>();
-	private static Long[] temposTotais = new Long[2];
+	private static long[] temposTotais = new long[2];
+	private static long tempoTotalPorT = 0;
+	private static long tempoInicial = 0;
+	private static long tempoFinal = 0;
+	private static long tempoTotalPrimeiraEstrategia = 0;
+	private static long tempoTotalSegundaEstrategia = 0;
 	
 	public static void main(String[] args) {
 		executarGuloso();
@@ -23,68 +28,49 @@ public class Main {
 			.forEach(entry -> System.out.println("Quantidade de Rotas: " + entry.getKey() + ", Tempo (em ms) por execução: " + entry.getValue()));
 		
 		System.out.println();
-		System.out.println("Tempo Total de execução: " + temposTotais[0] + " ms");
+		
+		hashMapSegundoGuloso.entrySet().stream()
+			.sorted(Map.Entry.comparingByKey())
+			.forEach(entry -> System.out.println("Quantidade de Rotas: " + entry.getKey() + ", Tempo (em ms) por execução: " + entry.getValue()));
+		
+		System.out.println();
+		System.out.println("Tempo Total da primeira estratégia gulosa: " + temposTotais[0] + " ms");
+		System.out.println("Tempo Total da segunda estratégia gulosa: " + temposTotais[1] + " ms");
 	}
 	
 	public static void executarGuloso() {
-		long tempoTotalPorT = 0, tempoTotal = 0;
-		
+		primeiraEstrategiaGulosa();
+		segundaEstrategiaGulosa();
+	}
+	
+	private static void primeiraEstrategiaGulosa() {
 		int T = 6;
 		int	dezVezesT = 6 * 10;
 		int primeiroValorDeT = T;
 		
-		System.out.println("Rodando Algoritmo Guloso...");
 		while(true) {
 			List<int[]> conjuntos = GeradorDeProblemas.geracaoDeRotas(10, T, 0.5);
-			int tamanho = conjuntos.size() * conjuntos.get(0).length;
-			int[] todasAsRotas = new int[tamanho];
-			
-			int indice = 0;
-			for(int[] rotas : conjuntos) {
-				for(int rota : rotas) {
-					todasAsRotas[indice++] = rota;
-				}
-			}
+			int[] todasAsRotas = gerarArrayUnidimensionalDeRotas(conjuntos);
 			
 			if(T > dezVezesT) break;
 			AlgoritmoGuloso algoritmoGuloso = new AlgoritmoGuloso(todasAsRotas, 3);
 			
-			List<Long> execucoes = new ArrayList<Long>();
+			List<Long> execucoesDaPrimeiraEstrategia = new ArrayList<Long>();
 			for(int i = 0; i < 10; i++) {
-				long tempoInicial = System.currentTimeMillis();
+				tempoInicial = System.currentTimeMillis();
 				algoritmoGuloso.distribuirRotasOrdenando();
-				long tempoFinal = System.currentTimeMillis(); 
+				tempoFinal = System.currentTimeMillis(); 
 				tempoTotalPorT = (tempoFinal - tempoInicial);
-				tempoTotal += tempoTotalPorT;
-				execucoes.add(tempoTotalPorT);
+				tempoTotalPrimeiraEstrategia += tempoTotalPorT;
+				execucoesDaPrimeiraEstrategia.add(tempoTotalPorT);
 			}
-			hashMapPrimeiroGuloso.put(T, execucoes);
+			hashMapPrimeiroGuloso.put(T, execucoesDaPrimeiraEstrategia);
 			T += primeiroValorDeT;
 		}
-		temposTotais[0] = tempoTotal;
+		temposTotais[0] = tempoTotalPrimeiraEstrategia;
 	}
 	
-	public static void guloso1(int[] todasAsRotas) {
-		AlgoritmoGuloso guloso = new AlgoritmoGuloso(todasAsRotas, 3);
-		guloso.distribuirRotasOrdenando();
-		
-		int ordemCaminhao = 1;
-		List<Caminhao> caminhoes = guloso.obterCaminhoes();
-		for(Caminhao caminhao : caminhoes) {
-			System.out.print("Rotas do " + ordemCaminhao + "º caminhão: ");
-			
-			for(Integer rota : caminhao.getRotas()) {
-				System.out.print(rota + "km; ");
-			}
-			System.out.println();
-			
-			System.out.println("Total de rotas: " + caminhao.totalDeRotas() + " km");
-			ordemCaminhao++;
-		}
-	}
-	
-	public static void guloso2(int[] rotasAsRotas) {
-		AlgoritmoGuloso guloso = new AlgoritmoGuloso(rotasAsRotas, 3);
+	private static void segundaEstrategiaGulosa() {
 		Comparator<Caminhao> comparador = new Comparator<Caminhao>() {
 			@Override
 			public int compare(Caminhao o1, Caminhao o2) {
@@ -92,10 +78,35 @@ public class Main {
 				else return 0;
 			}
 		};
-		guloso.distribuirRotasParaCaminhaoComMenosRotas(comparador);
+		int T = 6;
+		int	dezVezesT = 6 * 10;
+		int primeiroValorDeT = T;
 		
+		while(true) {
+			List<int[]> conjuntos = GeradorDeProblemas.geracaoDeRotas(10, T, 0.5);
+			int[] todasAsRotas = gerarArrayUnidimensionalDeRotas(conjuntos);
+			
+			if(T > dezVezesT) break;
+			AlgoritmoGuloso algoritmoGuloso = new AlgoritmoGuloso(todasAsRotas, 3);
+			
+			List<Long> execucoesDaSegundaEstrategia = new ArrayList<Long>();
+			for(int i = 0; i < 10; i++) {				
+				tempoInicial = System.currentTimeMillis();
+				algoritmoGuloso.distribuirRotasParaCaminhaoComMenosRotas(comparador);
+				tempoFinal = System.currentTimeMillis(); 
+				tempoTotalPorT = (tempoFinal - tempoInicial);
+				tempoTotalSegundaEstrategia += tempoTotalPorT;
+				execucoesDaSegundaEstrategia.add(tempoTotalPorT);
+			}
+			hashMapSegundoGuloso.put(T, execucoesDaSegundaEstrategia);
+			T += primeiroValorDeT;
+		}
+		temposTotais[1] = tempoTotalSegundaEstrategia;
+	}
+	
+	private static void mostrarDadosDoCaminhao(AlgoritmoGuloso algoritmoGuloso) {
 		int ordemCaminhao = 1;
-		List<Caminhao> caminhoes = guloso.obterCaminhoes();
+		List<Caminhao> caminhoes = algoritmoGuloso.obterCaminhoes();
 		for(Caminhao caminhao : caminhoes) {
 			System.out.print("Rotas do " + ordemCaminhao + "º caminhão: ");
 			
@@ -107,5 +118,18 @@ public class Main {
 			System.out.println("Total de rotas: " + caminhao.totalDeRotas() + " km");
 			ordemCaminhao++;
 		}
+	}
+	
+	private static int[] gerarArrayUnidimensionalDeRotas(List<int[]> conjuntos) {
+		int tamanho = conjuntos.size() * conjuntos.get(0).length;
+		int[] todasAsRotas = new int[tamanho];
+		
+		int indice = 0;
+		for(int[] rotas : conjuntos) {
+			for(int rota : rotas) {
+				todasAsRotas[indice++] = rota;
+			}
+		}
+		return todasAsRotas;
 	}
 }
