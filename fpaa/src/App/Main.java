ceeabeffd1782b32319bfa6d21e4d6c0c435de35
diogomsz <diogomrtsa@gmail.com>
2023-12-10 16:40:03 -0300

    Documentando classes

--- fpaa/src/App/Main.java
+++ fpaa/src/App/Main.java
@@ -3,69 +3,81 @@ package App;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 import Algoritmos.AlgoritmoGuloso;
 import Classes.Caminhao;
 import Geradores.GeradorDeProblemas;
 
+/**
+ * Classe que contém o Entry Point da nossa aplicação, ou seja, para executar o
+ * programa você precisa executar essa classe e o método "main" localizado nela.
+ */
 public class Main {
 	/**
-	 * HashMap que guarda o tamanho do conjunto com o tempo (em ms) de todas as 10
-	 * execuções para aquele conjunto para a primeira estratégia gulosa.
+	 * Propriedade privada da classe: HashMap que guarda o tamanho do conjunto com o
+	 * tempo (em ms) de todas as 10 execuções para aquele conjunto para a primeira
+	 * estratégia gulosa.
 	 */
 	private static Map<Integer, List<Long>> hashMapPrimeiroGuloso = new HashMap<Integer, List<Long>>();
 
 	/**
-	 * HashMap que guarda o tamanho do conjunto com o tempo (em ms) de todas as 10
-	 * execuções para aquele conjunto para a segunda estratégia gulosa.
+	 * Propriedade privada da classe: HashMap que guarda o tamanho do conjunto com o
+	 * tempo (em ms) de todas as 10 execuções para aquele conjunto para a segunda
+	 * estratégia gulosa.
 	 */
 	private static Map<Integer, List<Long>> hashMapSegundoGuloso = new HashMap<Integer, List<Long>>();
 
 	/**
-	 * Fila de caminhões que armazena dos dados de um determinado caminhão naquela
-	 * execução.
+	 * Propriedade privada da classe: Fila de caminhões que armazena dos dados de um
+	 * determinado caminhão naquela execução.
 	 */
-	private static Queue<List<Caminhao>> filaDeCaminhoes = new LinkedList<List<Caminhao>>();
+	private static List<List<Caminhao>> filaDeCaminhoes = new ArrayList<List<Caminhao>>();
 
 	/**
-	 * Array unidimensional de duas posições que guarda o tempo total gasto pela
-	 * primeira estratégia gulosa na primeira posição e o tempo total gasto pela
-	 * segunda estratégia gulosa na segunda posição.
+	 * Propriedade privada da classe: Array unidimensional de duas posições que
+	 * guarda o tempo total gasto pela primeira estratégia gulosa na primeira
+	 * posição e o tempo total gasto pela segunda estratégia gulosa na segunda
+	 * posição.
 	 */
 	private static long[] temposTotais = new long[2];
 
 	/**
-	 * Para cada tamanho T de conjunto, essa variável armazena o tempo total
-	 * temporariamente.
+	 * Propriedade privada da classe: Para cada tamanho T de conjunto, essa variável
+	 * armazena o tempo total temporariamente.
 	 */
 	private static long tempoTotalPorT = 0;
 
 	/**
-	 * Variável auxiliar que marca o início da execução do algoritmo.
+	 * Propriedade privada da classe: Variável auxiliar que marca o início da
+	 * execução do algoritmo.
 	 */
 	private static long tempoInicial = 0;
 
 	/**
-	 * Variável auxiliar que marca o fim da execução do algoritmo.
+	 * Propriedade privada da classe: Variável auxiliar que marca o fim da execução
+	 * do algoritmo.
 	 */
 	private static long tempoFinal = 0;
 
 	/**
-	 * Variável auxiliar que guarda o tempo total gasto na primeira estratégia
-	 * gulosa.
+	 * Propriedade privada da classe: Variável auxiliar que guarda o tempo total
+	 * gasto na primeira estratégia gulosa.
 	 */
 	private static long tempoTotalPrimeiraEstrategia = 0;
 
 	/**
-	 * Variável auxiliar que guarda o tempo total gasto na segunda estratégia
-	 * gulosa.
+	 * Propriedade privada da classe: Variável auxiliar que guarda o tempo total
+	 * gasto na segunda estratégia gulosa.
 	 */
 	private static long tempoTotalSegundaEstrategia = 0;
 
+	/**
+	 * Propriedade privada da classe: Entry point da nossa aplicação.
+	 * 
+	 * @param args Argumentos passados durante a chamda da nossa função.
+	 */
 	public static void main(String[] args) {
 		executarGuloso();
 
@@ -86,11 +98,12 @@ public class Main {
 		System.out.println("Tempo Total da segunda estratégia gulosa: " + temposTotais[1] + " ms");
 
 		System.out.println();
-		mostrarDadosDoCaminhao();
+		mostrarDadosDoCaminhao(4);
 	}
 
 	/**
-	 * Executa as duas estratégias gulosas.
+	 * Executa as duas estratégias gulosas, encapsulando seu funcionamento por meio
+	 * de dois métodos distintos.
 	 */
 	public static void executarGuloso() {
 		primeiraEstrategiaGulosa();
@@ -98,8 +111,8 @@ public class Main {
 	}
 
 	/**
-	 * Executa a primeira estratégia gulosa, aumentando o tamanho do conjunto de T
-	 * em T.
+	 * Executa a primeira estratégia gulosa, aumento o tamanho de T em T e
+	 * executando 10 vezes cada conjunto de tamanho T.
 	 */
 	private static void primeiraEstrategiaGulosa() {
 		int T = 6;
@@ -134,8 +147,8 @@ public class Main {
 	}
 
 	/**
-	 * Executa a segunda estratégia gulosa, aumentando o tamanho do conjunto de T
-	 * em T.
+	 * Executa a primeira estratégia gulosa, aumento o tamanho de T em T e
+	 * executando 10 vezes cada conjunto de tamanho T.
 	 */
 	private static void segundaEstrategiaGulosa() {
 		Comparator<Caminhao> comparador = new Comparator<Caminhao>() {
@@ -175,15 +188,19 @@ public class Main {
 	}
 
 	/**
-	 * Mostra os dados dos caminhões depois de executar uma estratégia gulosa.
+	 * Mostrar os dados dos caminhões, a quantidade de dados retornados será
+	 * flexível para não poluir o console.
 	 */
-	private static void mostrarDadosDoCaminhao() {
+	private static void mostrarDadosDoCaminhao(int quantidade) {
 		int ordemCaminhao = 1;
-		for (List<Caminhao> caminhoes : filaDeCaminhoes) {
+		for (int i = 0; i < quantidade; i++) {
+			List<Caminhao> caminhoes = filaDeCaminhoes.get(i);
 			for (Caminhao caminhao : caminhoes) {
+				if (caminhoes == null)
+					continue;
 				System.out.print("Rotas do " + ordemCaminhao + "º caminhão: ");
 
-				for (Integer rota : caminhao.getRotas()) {
+				for (int rota : caminhao.getRotas()) {
 					System.out.print(rota + "km; ");
 				}
 				System.out.println();
@@ -191,12 +208,14 @@ public class Main {
 				System.out.println("Total de rotas: " + caminhao.totalDeRotas() + " km");
 				ordemCaminhao++;
 			}
+			ordemCaminhao = 1;
+			System.out.println();
 		}
 	}
 
 	/**
-	 * Método responsável por gerar um array unidimensional de rotas, com base em
-	 * uma Lista de arrays unidimensionais.
+	 * Gerar um array unidimensional de rotas, com base em uma Lista de arrays
+	 * unidimensionais.
 	 * 
 	 * @param conjuntos Lista de arrays unidimensionais do tipo inteiro.
 	 * @return Array unidimensional de valores do tipo inteiro.
